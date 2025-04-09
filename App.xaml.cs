76f12c8151a149fece871a77bd8803e2afb2646b
DanarQusyairi <91103032+DanarQ@users.noreply.github.com>
2025-04-09 15:53:22 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	modified:   App.xaml.cs
    	modified:   MainWindow.xaml
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.dll
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.exe
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.pdb
    	modified:   obj/Debug/net9.0-windows/MainWindow.baml
    	modified:   obj/Debug/net9.0-windows/MainWindow.g.cs
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.AssemblyInfo.cs
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.AssemblyInfoInputs.cache
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.dll
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.g.resources
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.pdb
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_intg45b5_wpftmp.AssemblyInfo.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_intg45b5_wpftmp.AssemblyInfoInputs.cache
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_intg45b5_wpftmp.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_intg45b5_wpftmp.GlobalUsings.g.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_intg45b5_wpftmp.assets.cache
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_yuqefcez_wpftmp.AssemblyInfo.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_yuqefcez_wpftmp.AssemblyInfoInputs.cache
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_yuqefcez_wpftmp.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_yuqefcez_wpftmp.GlobalUsings.g.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_yuqefcez_wpftmp.assets.cache
    	modified:   obj/Debug/net9.0-windows/apphost.exe
    	modified:   obj/Debug/net9.0-windows/ref/ManajemenBarang.dll
    	modified:   obj/Debug/net9.0-windows/refint/ManajemenBarang.dll

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 
 namespace ManajemenBarang;
@@ -9,5 +11,18 @@ namespace ManajemenBarang;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // Set the culture to Indonesian (Indonesia)
+        var culture = new CultureInfo("id-ID");
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+
+        // Ensure the culture is applied to WPF framework elements
+        FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(System.Windows.Markup.XmlLanguage.GetLanguage(culture.IetfLanguageTag)));
+
+        base.OnStartup(e);
+    }
 }
 
