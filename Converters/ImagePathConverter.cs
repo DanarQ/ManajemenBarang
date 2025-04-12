1d07d7649f5bff5869986d6257b77a63775acbac
DanarQusyairi <91103032+DanarQ@users.noreply.github.com>
2025-04-12 18:51:21 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch V1.1
    Your branch is up to date with 'origin/V1.1'.
    
    Changes to be committed:
    	new file:   Assets/no-image.png
    	modified:   Converters/ImagePathConverter.cs
    	modified:   MainWindow.xaml
    	modified:   ManajemenBarang.csproj
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.dll
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.exe
    	modified:   bin/Debug/net9.0-windows/ManajemenBarang.pdb
    	modified:   obj/Debug/net9.0-windows/MainWindow.baml
    	modified:   obj/Debug/net9.0-windows/MainWindow.g.cs
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.AssemblyInfo.cs
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.AssemblyInfoInputs.cache
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.dll
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.g.resources
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.pdb
    	modified:   obj/Debug/net9.0-windows/ManajemenBarang.sourcelink.json
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.AssemblyInfo.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.AssemblyInfoInputs.cache
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.GlobalUsings.g.cs
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.assets.cache
    	new file:   obj/Debug/net9.0-windows/ManajemenBarang_ozg50psx_wpftmp.sourcelink.json
    	modified:   obj/Debug/net9.0-windows/apphost.exe
    	modified:   obj/Debug/net9.0-windows/ref/ManajemenBarang.dll
    	modified:   obj/Debug/net9.0-windows/refint/ManajemenBarang.dll

--- Converters/ImagePathConverter.cs
+++ Converters/ImagePathConverter.cs
@@ -9,39 +9,36 @@ namespace ManajemenBarang.Converters
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            try
             {
-                return null;
-            }
-
-            string fileName = value.ToString();
-            string directoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AssetGambar");
-
-            if (!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);
-            }
-
-            string fullPath = Path.Combine(directoryPath, fileName);
-            
-            if (File.Exists(fullPath))
-            {
-                try
+                if (value == null || string.IsNullOrEmpty(value.ToString()))
                 {
-                    var image = new BitmapImage();
-                    image.BeginInit();
-                    image.CacheOption = BitmapCacheOption.OnLoad;
-                    image.UriSource = new Uri(fullPath);
-                    image.EndInit();
-                    return image;
+                    return new BitmapImage(new Uri("/Assets/no-image.png", UriKind.Relative));
                 }
-                catch (Exception)
-                {
-                    return null;
-                }
-            }
 
-            return null;
+                string fileName = value.ToString();
+                string directoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AssetGambar");
+                string fullPath = Path.Combine(directoryPath, fileName);
+
+                if (!File.Exists(fullPath))
+                {
+                    return new BitmapImage(new Uri("/Assets/no-image.png", UriKind.Relative));
+                }
+
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                image.UriSource = new Uri(fullPath);
+                image.EndInit();
+                image.Freeze(); // Improves performance
+
+                return image;
+            }
+            catch (Exception)
+            {
+                return new BitmapImage(new Uri("/Assets/no-image.png", UriKind.Relative));
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
